b43d6a03c7ae269ac3c8a0c6ee7672029a28b0d3
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-25 11:09:12 -0300

    Add User Model

---
+++ Car-Rental/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Rental.Models;
+
+public class User
+{
+    public string UserId { get; set; }
+
+    [MaxLength(50)] public string FirstName { get; set; }
+
+    [MaxLength(50)] public string LastName { get; set; }
+
+    [MaxLength(255)]
+    [Column(TypeName = "varchar(255)")]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+    
+    [MaxLength(30)]
+    [Column(TypeName = "varchar(30)")]
+    public string PhoneNumber { get; set; }
+    
+    public DateTime? DateOfBirth { get; set; }
+    
+    [MaxLength(100)]
+    public string AddressOne { get; set; }
+    
+    [MaxLength(100)]
+    public string? AddressTwo { get; set; }
+    
+    [MaxLength(50)]
+    public string City { get; set; }
+    
+    [MaxLength(50)]
+    public string Country { get; set; }
+    
+    [MaxLength(50)]
+    public string LicenseNumber { get; set; }
+}
