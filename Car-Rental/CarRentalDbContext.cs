e69ade67d3c0e42052588dbd1186ea8411d31e2d
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-25 12:25:21 -0300

    Add Car Rental DbContext

---
+++ Car-Rental/CarRentalDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Car_Rental.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Rental;
+
+public class CarRentalDbContext : DbContext
+{
+    public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Location> Locations { get; set; }
+}
