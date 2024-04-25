903b93409c5e68bba7c5e16536aa321ad13dc67b
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-25 14:39:18 -0300

    Make Email column of the User Unique

--- Car-Rental/CarRentalDbContext.cs
+++ Car-Rental/CarRentalDbContext.cs
@@ -5,13 +5,20 @@ namespace Car_Rental;
 
 public class CarRentalDbContext : DbContext
 {
-    public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<User> Users { get; set; }
     public DbSet<Car> Cars { get; set; }
     public DbSet<Reservation> Reservations { get; set; }
     public DbSet<Location> Locations { get; set; }
+    public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+        
+    }
 }
--- Car-Rental/Migrations/20240425173108_MakeUserEmailUniqueMigration.Designer.cs
+++ Car-Rental/Migrations/20240425173108_MakeUserEmailUniqueMigration.Designer.cs
@@ -4,6 +4,7 @@ using Car_Rental;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Car_Rental.Migrations
 {
     [DbContext(typeof(CarRentalDbContext))]
-    partial class CarRentalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240425173108_MakeUserEmailUniqueMigration")]
+    partial class MakeUserEmailUniqueMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
