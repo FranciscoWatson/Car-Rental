b2332feb0b0ea985b6d46a9c4bd8787181eb82d6
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-25 12:22:52 -0300

    Add Car and Car Location Models

---
+++ Car-Rental/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Rental.Models;
+
+public class Car
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int CarId { get; set; }
+    
+    [MaxLength(50)] 
+    public string Make { get; set; }
+    
+    [MaxLength(50)] 
+    public string Model { get; set; }
+    
+    public int Year { get; set; }
+    
+    [MaxLength(50)] 
+    public string LicensePlate { get; set; }
+    
+    public bool Available { get; set; }
+    
+    public List<Reservation> Reservations { get; set; }
+    
+    
+    public int LocationId { get; set; }
+    public Location Location { get; set; }
+
+}
---
+++ Car-Rental/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Rental.Models;
+
+public class Location
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int LocationId { get; set; }
+    
+    [MaxLength(50)]
+    public string City { get; set; }
+
+    [MaxLength(50)]
+    public string Country { get; set; }
+
+
+}
