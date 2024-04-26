5f30bd5433d3c09a5fd409760501255465db608c
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-26 12:35:09 -0300

    Add logout Page

---
+++ Car-Rental/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Rental.Pages;
+
+public class Logout : PageModel
+{
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await HttpContext.SignOutAsync("MyCookieAuth");
+        return RedirectToPage("/Index");
+    }
+}
