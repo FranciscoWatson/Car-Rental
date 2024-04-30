f69b133de3c87e0b19b1ace6193a7a9653ac8b62
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-30 13:30:23 -0300

    Add MainPage for the User

---
+++ Car-Rental/Pages/MainPage.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Car_Rental.DTOs;
+using Car_Rental.Models;
+using Car_Rental.Pages.InputModels;
+using Car_Rental.Repositories;
+using Car_Rental.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Rental.Pages;
+
+[Authorize]
+public class MainPage : PageModel
+{
+    private readonly ICarService _carService;
+
+    
+    public MainPage(ICarService carService)
+    {
+        _carService = carService;
+    }
+    
+    [BindProperty]
+    public MainPageInputModel Input { get; set; }
+    
+    public IEnumerable<CarDto> AvailableCars { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        
+        AvailableCars = await _carService.GetAvailableCarsAsync(Input.City, Input.Country, Input.Date);
+        
+        return Page();
+    }
+    
+}
