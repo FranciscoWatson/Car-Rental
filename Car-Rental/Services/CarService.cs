b649a1d081e2de278210a58317cfba86d9da7340
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-30 13:28:51 -0300

    Add CarService

---
+++ Car-Rental/Services/CarService.cs
@@ -0,0 +1,39 @@
+﻿using Car_Rental.DTOs;
+using Car_Rental.Repositories;
+
+namespace Car_Rental.Services;
+
+public class CarService : ICarService
+{
+    private readonly ICarRepository _carRepository;
+    private readonly IReservationRepository _reservationRepository;
+
+    public CarService(ICarRepository carRepository, IReservationRepository reservationRepository)
+    {
+        _carRepository = carRepository;
+        _reservationRepository = reservationRepository;
+    }
+
+    public async Task<IEnumerable<CarDto>> GetAvailableCarsAsync(string city, string country, DateTime? date)
+    {
+        var cars = await _carRepository.GetAvailableCarsAsync(city, country, date);
+        var carDtos = new List<CarDto>();
+        
+        foreach (var car in cars)
+        {
+            var nextAvailableDate = await _reservationRepository.GetNextAvailableDateAsync(car.CarId, date);
+            carDtos.Add(new CarDto
+            {
+                CarId = car.CarId,
+                Make = car.Make,
+                Model = car.Model,
+                Year = car.Year,
+                LocationCity = car.Location.City,
+                LocationCountry = car.Location.Country,
+                NextAvailableDate = nextAvailableDate
+            });
+        }
+
+        return carDtos;
+    }
+}
---
+++ Car-Rental/Services/ICarService.cs
@@ -0,0 +1,8 @@
+﻿using Car_Rental.DTOs;
+
+namespace Car_Rental.Services;
+
+public interface ICarService
+{
+    Task<IEnumerable<CarDto>> GetAvailableCarsAsync(string city, string country, DateTime? date);
+}
