38fc2cddc40212bafe3abb4d53f56f7f94862640
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-30 16:54:55 -0300

    Add ReservationForCreationDto

---
+++ Car-Rental/DTOs/ReservationForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using Car_Rental.Enums;
+
+namespace Car_Rental.DTOs;
+
+public class ReservationForCreationDto
+{
+    public int CarId { get; set; }
+    public int UserId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public ReservationStatus ReservationStatus { get; set; }
+    
+}
