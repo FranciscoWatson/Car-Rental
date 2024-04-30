57bfc2b1e77d1c6c8745de42a155d681b7a02d68
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-30 13:26:53 -0300

    Add CarDto including NextAvailableDate

---
+++ Car-Rental/DTOs/CarDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Car_Rental.DTOs;
+
+public class CarDto
+{
+    public int CarId { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string LocationCity { get; set; }
+    public string LocationCountry { get; set; }
+    public DateTime? NextAvailableDate { get; set; }
+}
