6d93baa59d58b77fc014c79a52817b26ff98e8f9
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-30 13:28:14 -0300

    Add MainPageInputModel

---
+++ Car-Rental/Pages/InputModels/MainPageInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental.Pages.InputModels;
+
+public class MainPageInputModel
+{
+    [Required(ErrorMessage = "City is required.")]
+    public string City { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Country is required.")]
+    public string Country { get; set; } = string.Empty;
+
+    [DataType(DataType.Date)]
+    public DateTime? Date { get; set; }
+}
