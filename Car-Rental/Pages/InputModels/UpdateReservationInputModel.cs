a933732989903b283e30f8a77812b929d10728df
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-05-08 18:21:48 -0300

    Add UpdateReservationInputModel

---
+++ Car-Rental/Pages/InputModels/UpdateReservationInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental.Pages.InputModels;
+
+public class UpdateReservationInputModel
+{
+    [Required]
+    public int ReservationId { get; set; }
+    [Required]
+    public int CarId { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndDate { get; set; }
+
+}
