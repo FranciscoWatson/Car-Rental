c0c1565340adb35260f850125b717b2ba707519f
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-05-10 13:12:04 -0300

    Add Location Service

---
+++ Car-Rental/Services/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Car_Rental.Services;
+
+public interface ILocationService
+{
+    Task<List<SelectListItem>> GetCitiesByCountryAsync(string country);
+    Task<List<SelectListItem>> GetCountriesAsync();
+}
---
+++ Car-Rental/Services/LocationService.cs
@@ -0,0 +1,35 @@
+﻿using Car_Rental.Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Car_Rental.Services;
+
+public class LocationService : ILocationService
+{
+    private readonly ILocationRepository _locationRepository;
+    
+    public LocationService(ILocationRepository locationRepository)
+    {
+        _locationRepository = locationRepository;
+    }
+    
+    public async Task<List<SelectListItem>> GetCountriesAsync()
+    {
+        var countries = await _locationRepository.GetCountriesAsync();
+        return countries.Select(country => new SelectListItem
+        {
+            Value = country,
+            Text = country
+        }).ToList();
+    }
+    
+    public async Task<List<SelectListItem>> GetCitiesByCountryAsync(string country)
+    {
+        var cities = await _locationRepository.GetCitiesByCountryAsync(country);
+        return cities.Select(city => new SelectListItem
+        {
+            Value = city,
+            Text = city
+        }).ToList();
+    }
+
+}
